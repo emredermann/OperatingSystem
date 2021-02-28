8b71f5711557cc65d52605c0bde7d57c2fc13fdf
emredermann <emre.derman@ug.bilkent.edu.tr>
2021-02-28 15:00:38 +0300

    Timing variables added.

--- consumer.c
+++ consumer.c
@@ -4,6 +4,9 @@
 
 int main(int argc, char* argv[]) {
    int M ;
+   struct timeval startTime;
+    struct timeval endTime;
+    gettimeofday(&startTime,NULL);
    if (argc > 1) {
         M = atoi(argv[1]);  
     }
@@ -13,4 +16,7 @@ int main(int argc, char* argv[]) {
         scanf(" %c",&tmp[bytesRead]); 
         bytesRead += 1;
     }
+    gettimeofday(&endTime,NULL);
+    printf("Run time as sc : %ld \n",(endTime.tv_sec-startTime.tv_sec));
+    printf("Run time as micro sec : %ld \n",(endTime.tv_usec-startTime.tv_usec));
 }
--- producer.c
+++ producer.c
@@ -6,11 +6,13 @@
 const char alphabet [26] = "abcdefghijklmnopqrstuvwxyz";
 int main(int argc, char* argv[]) {
  int M;
+ 
  if (argc > 1) {
         M = atoi(argv[1]); // get the value of M (will be used in experiments)
     }
     for(int i = 0; i < M; i++){
         printf("%c \n",alphabet[rand() % 26]);
     }
+
 return 0;
 }
